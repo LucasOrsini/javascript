849ab57599d792888d1d080bb701b0cd20c2a149
LucasOrsini <74557947+LucasOrsini@users.noreply.github.com>
2025-05-07 20:26:34 -0300

    1

---
+++ curso-js/aula08ex/ex016/script.js
@@ -0,0 +1,13 @@
+function contar(){
+    let ini = document.getElementById('txtini')
+    let fim = document.getElementById('txtfim')
+    let passo = document.getElementById('txtpas')
+    let res = document.getElementById('res')
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        alert('[ERRO] Faltam dados !')
+
+    }else{
+        res.innerText
+    }
+}
