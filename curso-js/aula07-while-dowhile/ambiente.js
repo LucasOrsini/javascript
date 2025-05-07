c277d6c2f23e638d16704306c5c37f6edb6bb200
LucasOrsini <74557947+LucasOrsini@users.noreply.github.com>
2025-05-07 18:55:17 -0300

    Exercício sobre o laço de repetição "for"

--- curso-js/aula07-while-dowhile/ambiente.js
+++ curso-js/aula07-while-dowhile/ambiente.js
@@ -4,13 +4,18 @@ while (c <= 50){
     console.log(`Hello World!`)
     c++ // c = c + 1
 }
-//outra forma de fazer o mesmo código acima
-/*  
+  
+// do while: estrutura de repetição com teste lógico no final
     do{
      console.log("Hello World!");
      c++ // c = c + 1
      } while (c <= 50)
-*/
+
+// for: estrutura de repetição com variável de control (inicialização, teste lógico e incremento)
+for (var c = 1; c <= 50; c++){
+    console.log("Hello World!");
+}
+
 
 
 
