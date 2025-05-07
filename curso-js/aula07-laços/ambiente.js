ea6183e7d8a1d8e00f53bbf93adae3cf09a234d3
LucasOrsini <74557947+LucasOrsini@users.noreply.github.com>
2025-05-07 14:01:42 -0300

    Estudo sobre laço de repetição iniciado

---
+++ curso-js/aula07-laços/ambiente.js
@@ -0,0 +1,24 @@
+// while: estrutura de repetição com teste lógico no início
+var c = 1
+while (c <= 50){
+    console.log(`Hello World!`)
+    c++ // c = c + 1
+}
+//outra forma de fazer o mesmo código acima
+/*  
+    do{
+     console.log("Hello World!");
+     c++ // c = c + 1
+     } while (c <= 50)
+*/
+
+
+
+/*
+console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+*/
