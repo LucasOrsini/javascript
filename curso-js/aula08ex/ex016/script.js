aa85d1963d4447a6d610d95198c7b3855d74ec9d
LucasOrsini <74557947+LucasOrsini@users.noreply.github.com>
2025-05-07 21:07:45 -0300

    Exercício 16 finalizado

--- curso-js/aula08ex/ex016/script.js
+++ curso-js/aula08ex/ex016/script.js
@@ -1,13 +1,33 @@
 function contar(){
-    let ini = document.getElementById('txtini')
-    let fim = document.getElementById('txtfim')
-    let passo = document.getElementById('txtpas')
-    let res = document.getElementById('res')
+let ini = document.getElementById('txtini')
+let fim = document.getElementById('txtfim')
+let pas = document.getElementById('txtpas')
+let res = document.getElementById('res')
 
-    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        alert('[ERRO] Faltam dados !')
+if (ini.value.lenght == 0 || fim.value.lenght == 0 || pas.value.lenght == 0 ){
+    alert('[ERRO] Números inválidos')
 
-    }else{
-        res.innerText
+}else{
+    res.innerHTML = `Contando: <br>`
+    let i = Number(ini.value)
+    let f = Number(fim.value)
+    let p = Number(pas.value)
+
+    if(p <= 0){
+        alert('Passo inválido! Considerando passo 1')
+        p = 1
+    }
+
+    if(i < f){
+        for(let c = i; c <= f; c += p){
+            res.innerHTML += `${c} \u{1F449}`
+        }
+    } else{
+        for(let c = i; c >= f; c -= p){
+            res.innerHTML += `${c} \u{1F449}`
+        }
+    }
+
+    res.innerHTML += `\u{1F3C1}`
     }
 }
