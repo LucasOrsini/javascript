afb4c080508df6713568d71425f588eb94d15d23
LucasOrsini <74557947+LucasOrsini@users.noreply.github.com>
2025-05-13 12:50:57 -0300

    Exercício finalizado

--- curso-js/aula010ex/script.js
+++ curso-js/aula010ex/script.js
@@ -26,6 +26,7 @@ function adicionar(){
         let item = document.createElement('Option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
+        res.innerHTML = ''
     }else{
         alert('Valor inválido ou já adicionado na lista!')
     }
@@ -34,5 +35,31 @@ function adicionar(){
 }
 
 function finalizar(){
+    if(valores.length == 0){
+        alert('Adicione valores antes de finalizar!')
+
+    }else{
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]
+
+        }
+        media = soma / total
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menorr valor informado foi ${menor}`
+        res.innerHTML += `<p>Somando todos o valores, temos ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media} </p>`
+    }
 
 }
