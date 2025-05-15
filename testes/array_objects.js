adcf65a04e9bca351a0e4e962c3671f0e5391f57
LucasOrsini <74557947+LucasOrsini@users.noreply.github.com>
2025-05-14 21:39:44 -0300

    Teste com array

---
+++ testes/array_objects.js
@@ -0,0 +1,10 @@
+const pessoa = ["Lucas", "Orsini", "Estudante"] // Matriz / Array
+const lucas = {sexo: "Masculino", idade: 20} // Objeto / Object
+
+console.log(pessoa, lucas)
+
+console.log(pessoa[pessoa.length -1]) // Mostra o valor do último índice
+
+console.log(Array.isArray(pessoa)) // Se for um array (true), se não for (false)
+
+console.log(Array.isArray(lucas))
